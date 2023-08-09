f2a01232657f47b5d52be8028b81f9b5dc049886
shahabhussain09 <shahabkhan0981233@gmail.com>
2023-08-09 11:35:44 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
+  menu.onclick = function () {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+  };
+});
 
-menu.onclick(()=>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+// menu.classList.toggle('fa-times');
+// navbar.classList.toggle('active');
 
-})
-    // menu.classList.toggle('fa-times');
-    // navbar.classList.toggle('active');
-
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-} 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
